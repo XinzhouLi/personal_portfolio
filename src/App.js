5af83492914bbbcb674a4f2582b3254b4d883aa5
XinzhouLi <48045064+XinzhouLi@users.noreply.github.com>
2023-10-23 00:39:26 -0600

    Skills part finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {NavBar} from "./components/NavgationBar"
 import "./App.css"
 import {Banner} from "./components/Banner";
+import {Skills} from "./components/Skills";
+
 function App() {
     return (
         <>
             <NavBar/>
             <Banner/>
+            <Skills/>
         </>
-
     );
 }
 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,59 +4,58 @@ import {ArrowRightCircle} from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
 import {useEffect, useState} from "react";
 export const Banner = () =>  {
-    const toRotate = [" Deep Learning Developer", " Full-stack Developer"]
+    const toRotate = ["A DL Developer.  ", "A Full-Stack Developer.  "]
     let [wordIndex,setWordIndex] = useState(0)
     let [charIndex, setCharIndex] = useState(1)
     const [isTyping, setIsTyping] = useState(true)
     const [text, setText] = useState('')
     const [typingDelta, setTypingDelta]=useState(0)
-    const period = 2000
 
     useEffect(
         () =>{
-            let fullText = toRotate[wordIndex]
-            let updateText = ''
-            if (isTyping){
-                setTypingDelta(300 - Math.random()*100)
-                updateText = fullText.substring(0, charIndex)
-                setCharIndex(charIndex + 1)
-            }
-            else if(!isTyping){
-                setTypingDelta(prevState => prevState/2)
-                setCharIndex(charIndex - 1)
-                updateText = fullText.substring(0,charIndex)
-            }
-            const timeOut = setTimeout(
-                ()=>{
-                    setText(updateText)
-                },typingDelta
-            );
-
-            if(charIndex === fullText.length && isTyping){
-                setIsTyping(false)
-                setCharIndex(charIndex - 1)
-            }
-            else if(charIndex === 0 && !isTyping){
-                setWordIndex((wordIndex + 1)%toRotate.length)
-                setCharIndex(1)
-                setIsTyping(true)
-            }
-            console.log(updateText, charIndex, wordIndex, isTyping, typingDelta)
-
-
-
-        return()=>{
-            clearTimeout(timeOut)
-        }
-
+            simulateTyping()
         },[text]
     )
+
+    const simulateTyping = ()=> {
+        let fullText = toRotate[wordIndex]
+        let updateText = ''
+        if (isTyping){
+            setTypingDelta(400 - Math.random()*200)
+            updateText = fullText.substring(0, charIndex)
+            setCharIndex(charIndex + 1)
+        }
+        else if(!isTyping){
+            setCharIndex(charIndex - 1)
+            updateText = fullText.substring(0,charIndex)
+        }
+        const timeOut = setTimeout(
+            ()=>{
+                setText(updateText)
+            },typingDelta
+        );
+
+        if(charIndex === fullText.length && isTyping){
+            setTypingDelta(prevState => prevState/4)
+            setIsTyping(false)
+            setCharIndex(charIndex - 1)
+        }
+        else if(charIndex === 0 && !isTyping){
+            setWordIndex((wordIndex + 1)%toRotate.length)
+            setCharIndex(1)
+            setIsTyping(true)
+        }
+        // console.log(updateText, charIndex, wordIndex, isTyping, typingDelta)
+        return ()=>{
+            clearTimeout(timeOut)
+        }
+    }
 
     return(
         <section className="banner" id="home">
             <Container>
                <Row className="align-items-center">
-                   <Col xs={12} md={6} xl={7}>
+                   <Col xs={12} md={7} xl={7} >
                         <span className="tagline">
                             Welcome to my Portfolio
                         </span>
@@ -67,19 +66,18 @@ export const Banner = () =>  {
                        </h1>
                        <p>
                            Introduction about me
+
                        </p>
                        <button >
                            Let's connect!
                            <ArrowRightCircle/>
                        </button>
                    </Col>
-                   <Col xs={12} md={6} xl={5}>
+                   <Col xs={12} md={5} xl={5}>
                        <img src={headerImg} alt="Header"/>
                    </Col>
                </Row>
             </Container>
-
-
         </section>
 
 
